a3c36c98022baf7bd6f20da8635a5a81d6c6f341
Oksana Alekseienko <oksana@rtacademy.net>
2022-05-04 10:56:23 +0300

    js: 6.6 func capitalize

---
+++ 06/06/script.js
@@ -0,0 +1,42 @@
+function capitalize( cityName )
+{
+    if( typeof cityName === 'string' )
+    {
+        let lower = cityName.toLowerCase();
+        let first = lower[ 0 ].toUpperCase();
+
+        return first + lower.substring( 1 );
+    }
+    else if( typeof cityName === 'object' )
+    {
+        let cityArray = [];
+        let result, i;
+
+        for( i = 0; i < cityName.length; i++ )
+        {
+            let lower = cityName[ i ].toString().toLowerCase();
+            let first = lower[ 0 ].toUpperCase();
+            result = first + lower.substring( 1 );
+            cityArray.push( result );
+        }
+
+        return cityArray;
+    }
+    else if( typeof cityName === 'number' )
+    {
+        let lower = cityName.toString().toLowerCase();
+        let first = lower[ 0 ].toUpperCase();
+        first + lower.substring( 1 );
+
+        return first + lower.substring( 1 );
+    }
+    else
+    {
+        console.log( "Введіть щось нормальне!" );
+    }
+}
+
+let cityNameCapitalized = capitalize( ["лЬвів", "одеСА", "хАРКіВ", "стОКГольМ"] );
+// ще варіанти: ["лЬвів", "одеСА", "хАРКіВ", "стОКГольМ"]
+// [ "привіт", [ [ "cвіт" ] ] ]
+console.log( cityNameCapitalized );
