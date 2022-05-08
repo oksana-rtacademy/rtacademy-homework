9c1144b8badd6fc80dc7d5a3bcc0628fd00d19b0
Oksana Alekseienko <oksana@rtacademy.net>
2022-05-08 11:55:15 +0300

    js: 6.4 update

--- 06/04/script.js
+++ 06/04/script.js
@@ -1,6 +1,6 @@
 // оригінал
 let age = 15,
-    withParents = false;
+    withParents = true;
 
 let accessAllowed = ( age > 18 ) ? 'Yes' : ( withParents ? 'Yes' : 'No' );
 console.info( accessAllowed );
@@ -9,7 +9,7 @@ console.info( accessAllowed );
 // 1й варіант
 let accessAllowed1;
 let age1 = 15;
-let withParents1 = false;
+let withParents1 = true;
 
 if( age1 < 18 && withParents1 === false )
 {
@@ -25,10 +25,10 @@ console.log( 'accessAllowed1 = ' + accessAllowed1 );
 
 // 2й варіант
 let age2 = 15,
-    withParents2 = false,
+    withParents2 = true,
     accessAllowed2 = 'No';
 
-if( age2 > 18 || withParents2 === true )
+if( age2 > 18 || withParents2 )
 {
     accessAllowed2 = 'Yes';
 }
